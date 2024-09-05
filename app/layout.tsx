2539e27b33daf11213c808f58348c871ad22efa2
Abdul Rojak <abdulrozaaq5@outlook.co.id>
2024-09-06 00:44:37 +0700

    "push pertama"

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="id-ID" data-theme="light">
       <body className={inter.className}>{children}</body>
     </html>
   );
