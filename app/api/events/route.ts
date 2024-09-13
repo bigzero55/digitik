40386a87e85e45c5d3af26b1cd32138d1177817f
Abdul Rojak <122954244+bigzero55@users.noreply.github.com>
2024-09-14 06:05:03 +0700

    update app/api/events/route.ts

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,12 +1,11 @@
 import { NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
 
-// Handle GET request
 export async function GET() {
   const token = cookies().get("token")
   const url = process.env.BE_URL
   try {
-    const response = await fetch(url+"api/events", {
+    const response = await fetch(`${url}/api/events`, {
       method: "GET", 
       headers: {
         headers: { 
@@ -29,36 +28,27 @@ export async function GET() {
 // Handle POST request (to add a new item)
 export async function POST(req: Request) {
   const body = await req.json();
-  const newItem = { id: items.length + 1, name: body.name };
-  items.push(newItem);
-  
-  return NextResponse.json({ message: "Item added", item: newItem });
-}
+  const token = cookies().get("token")
+  const url = process.env.BE_URL
 
-// Handle PUT request (to update an existing item)
-export async function PUT(req: Request) {
-  const body = await req.json();
-  const item = items.find(i => i.id === body.id);
-  
-  if (!item) {
-    return NextResponse.json({ message: "Item not found" }, { status: 404 });
+  try {
+    const response = await fetch(`${url}/api/events`, {
+      method: "POST", 
+      headers: {
+        headers: { 
+          'Content-Type': 'application/json',
+          "Authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify(body)
+      }
+    })
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    const data = await response.json();
+    return NextResponse.json(data)
+  } catch (error) {
+    return NextResponse.json(error)
   }
-
-  item.name = body.name;
-  
-  return NextResponse.json({ message: "Item updated", item });
-}
-
-// Handle DELETE request (to delete an item)
-export async function DELETE(req: Request) {
-  const body = await req.json();
-  const itemIndex = items.findIndex(i => i.id === body.id);
-  
-  if (itemIndex === -1) {
-    return NextResponse.json({ message: "Item not found" }, { status: 404 });
-  }
-
-  items.splice(itemIndex, 1); // Remove the item
-  
-  return NextResponse.json({ message: "Item deleted" });
 }
