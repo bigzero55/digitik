1b53414d1bfcd5660aeac7c1f4df3b9a6f64096b
Abdul Rojak <122954244+bigzero55@users.noreply.github.com>
2024-09-14 10:12:05 +0700

    update app/api/participants/route.ts

---
+++ app/api/participants/route.ts
@@ -0,0 +1,54 @@
+import { NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+
+export async function GET() {
+  const token = cookies().get("token")
+  const url = process.env.BE_URL
+  try {
+    const response = await fetch(`${url}/api/participants`, {
+      method: "GET", 
+      headers: {
+        headers: { 
+          'Content-Type': 'application/json',
+          "Authorization": `Bearer ${token}`
+        },
+      }
+    })
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    const data = await response.json();
+    return NextResponse.json(data)
+  } catch (error) {
+    return NextResponse.json(error)
+  }
+}
+
+// Handle POST request (to add a new item)
+export async function POST(req: Request) {
+  const body = await req.json();
+  const token = cookies().get("token")
+  const url = process.env.BE_URL
+
+  try {
+    const response = await fetch(`${url}/api/participants`, {
+      method: "POST", 
+      headers: {
+        headers: { 
+          'Content-Type': 'application/json',
+          "Authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify(body)
+      }
+    })
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    const data = await response.json();
+    return NextResponse.json(data)
+  } catch (error) {
+    return NextResponse.json(error)
+  }
+}
