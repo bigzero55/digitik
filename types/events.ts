fc297fa2104ae1f763a8e27778d748022be3e2c3
Abdul Rojak <122954244+bigzero55@users.noreply.github.com>
2024-09-15 22:15:32 +0700

    update types/events.ts

--- types/events.ts
+++ types/events.ts
@@ -1,7 +1,13 @@
-export interface Event {
+
+export type Event = {
   id: number;
+  user_id: number;
   title: string;
-  description: string;
-  date: string;
+  unix: string;
+  description?: string; // Nullable field
+  date: string; // Date format
+  price: number;
   capacity: number;
-}
+  location?: string; // Nullable field
+  created_at: string; // Datetime format
+};
