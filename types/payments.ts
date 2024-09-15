41ebe3e962f04b01abd54d4ab8eec9e3b80f5823
Abdul Rojak <122954244+bigzero55@users.noreply.github.com>
2024-09-15 22:21:53 +0700

    update types/payments.ts

--- types/payments.ts
+++ types/payments.ts
@@ -1,5 +1,4 @@
 
-// Type untuk tabel payments
 export type Payment = {
   id: number;
   user_id: number;
@@ -7,5 +6,5 @@ export type Payment = {
   payment_date: string; // Datetime format
   amount: number; // Decimal type handled as number in TypeScript
   status: string;
-  created_at: string; // Datetime format
+  created_at: string; // Datetime 
 };
