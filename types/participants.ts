ea8ba0339d128252a0b4082eff3e906c939e4659
Abdul Rojak <122954244+bigzero55@users.noreply.github.com>
2024-09-15 22:17:47 +0700

    update types/participants.ts

--- types/participants.ts
+++ types/participants.ts
@@ -1,6 +1,10 @@
-export interface Participant {
+
+export type Participant = {
   id: number;
+  user_id: number;
   name: string;
-  email: string;
+  unix: string;
   phone: string;
-}
+  email: string;
+  created_at: string; // Datetime format
+};
